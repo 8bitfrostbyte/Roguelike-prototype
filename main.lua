e9d6739628125eb56bdd10dc4096bff43c587d65
Mobile User <mobile@localhost>
2026-02-04 16:08:14 +0000

    save local changes at 2026-02-04 16:08:14

--- main.lua
+++ main.lua
@@ -11,13 +11,15 @@ end
 function love.update()
 
 -- Handles x movement
-
-
+if love.keypressed		== "right" then
+x = x + speed
+end
+end
 -- Handles Y movement
 
 
 function love.draw()
 
 love.graphics.rectangle("fill", x , y, 100, 100)
-love.graphics.circle("line", x, y, 100, 100)
+
 end
