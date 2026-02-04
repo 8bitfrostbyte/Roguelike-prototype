825b057c1a242e535796a90cfb2fe23e5f113d6f
Mobile User <mobile@localhost>
2026-02-04 13:43:50 +0000

    save local changes at 2026-02-04 13:43:50

--- main.lua
+++ main.lua
@@ -1,10 +1,15 @@
 function love.load()
 
+x = 100
+speed = 2
+
 end
 
 
 
 function love.update()
+
+x = x + 5 + speed
 
 end
 
@@ -12,6 +17,6 @@ end
 
 function love.draw()
 
-love.graphics.rectangle("fill", 100, 50, 100, 100)
+love.graphics.rectangle("fill", x , 50, 100, 100)
 
 end
