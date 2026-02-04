3d904958215b0fa37dfff0ed49a1d6291c4b06c3
Mobile User <mobile@localhost>
2026-02-04 14:03:50 +0000

    save local changes at 2026-02-04 14:03:50

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 function love.load()
 
 x = 100
+y = 100
 speed = 0.1
 
 end
@@ -8,8 +9,12 @@ end
 
 
 function love.update()
+
 if love.keyboard.isDown("right") then
 	x = x + 5 + speed
+
+elseif love.keyboard.isDown("left") then
+x = x -5 + speed
 	end
 end
 
@@ -17,6 +22,6 @@ end
 
 function love.draw()
 
-love.graphics.rectangle("fill", x , 50, 100, 100)
+love.graphics.rectangle("fill", x , y, 100, 100)
 
 end
