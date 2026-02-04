03247f7753efaf2979369e6b58b011afc0a54761
Mobile User <mobile@localhost>
2026-02-04 16:03:14 +0000

    save local changes at 2026-02-04 16:03:14

--- main.lua
+++ main.lua
@@ -11,24 +11,9 @@ end
 function love.update()
 
 -- Handles x movement
-if love.keyboard.isDown("right") then
-	x = x + 5 + speed
 
-elseif love.keyboard.isDown("left") then
-x = x -5 + speed
-	end
 
 -- Handles Y movement
-if love.keyboard.isDown("up") then
-y = y - 5 + speed
-
-elseif love.keyboard.isDown("down") then
-y = y + 5 + speed
-	end
-
-
-end
-
 
 
 function love.draw()
